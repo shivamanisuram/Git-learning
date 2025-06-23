2ac6262a3ff2115156c470e1addf23465e2c7207
SHIVAMANI REDDY <shivamanisuram@gmail.com>
2025-06-23 11:17:12 +0530

    payment updated afte footer

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ console.log(button);
 const login="login added";
 console.log(login);
 
+<<<<<<< HEAD
 const footer="footer added in our website"
 console.log(footer);
+=======
+const payment="payment added";
+console.log(payment);
+>>>>>>> 184a945 (payment)
